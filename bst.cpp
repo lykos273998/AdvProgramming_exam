346258053069cbe3aef510df31d175f441632a9b
lykos273998 <francy273998@gmail.com>
2021-04-29 15:32:10 +0200

    added comments

--- bst.cpp
+++ bst.cpp
@@ -22,7 +22,7 @@ class bst
     //iterator fwd declaration
     struct iterator;
     struct const_iterator;
-    node* root;
+    node* root = nullptr;
     
     
     //begin and end iterators
@@ -73,13 +73,19 @@ class bst
 
     //constructor for empty bst
     bst(){};
+
+    //constructor from a node
     bst(node* new_node){
         root = new_node;
     };
 
+    bst(node new_node){
+        root = &new_node;
+    };
     //copy constructor
     
     bst(const bst& bst_to_copy_from){
+        delete_all_nodes(root);
         root = copy_all_nodes(bst_to_copy_from.root);
     }
 
@@ -88,7 +94,19 @@ class bst
             return *(new bst(bst_to_copy_from));
             }
 
+    //move ctor
+    bst(bst&& bst_to_move){
+        //steal root
+        root = std::move(bst_to_move.root);
+    };
+    //move assignment
+    bst& operator=(bst&& bst_to_move){
+        //steal root
+        root = bst_to_move.root;
+        bst_to_move.root = nullptr;
 
+    };
+    //utility functions
     //recursively delete all the nodes
     ~bst(){
         delete_all_nodes(root);
@@ -97,6 +115,7 @@ class bst
     //aux function to copy and delete nodes
     //exploit recursion
     void delete_all_nodes(node* current_node){
+        if (current_node == nullptr)return;
         if (current_node -> LEFT_child != nullptr) delete_all_nodes(current_node -> LEFT_child );
         if (current_node -> RIGHT_child != nullptr) delete_all_nodes(current_node -> RIGHT_child);
         delete(current_node);
@@ -114,13 +133,6 @@ class bst
             copied_node -> RIGHT_child = copy_all_nodes(current_node -> RIGHT_child);
             copied_node -> RIGHT_child -> parent = copied_node;}
         return copied_node;
-    }
-
-    void print(){
-        for(auto n = begin(); n != end(); ++n){
-            std::cout << n -> KEY << "  " << n -> VAL << std::endl;
-        }
-
     }
 
     friend
@@ -354,7 +366,7 @@ int main(int argc, char** argv){
 
 
     auto bb2 = bb;
-    auto n_copy = new bst<int,int>::node(*n8);
+    
 
     //delete bb;
     //bb -> root = n8;
